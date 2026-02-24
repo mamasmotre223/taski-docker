b56dff22f9171f51324a81486442d43f99503c79
Nikita Lebedev <mamasmotre223@gmail.com>
2026-02-24 04:28:06 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
